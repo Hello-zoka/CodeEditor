ef36d4d9d457f93c3ff6c4a5db16f29c53888737
Yurij Zaytsev <77232884+Hello-zoka@users.noreply.github.com>
2023-03-27 21:30:44 +0200

    implemented full application

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,86 +1,107 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 //import kotlinx.coroutines.DefaultExecutor.delay
 import kotlinx.coroutines.delay
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.PrintWriter
-import java.nio.charset.StandardCharsets
 
+/**
+ * Class for working with code text
+ */
+class CodeTranformer() {
+    private val keyWords =
+        listOf("for", "while", "do", "if", "else", "when", "new", "throw", "var", "val", "fun")
+    private val separators = listOf(',', ';', '(', ')', '{', '}')
 
-//@Composable
-//fun LineNumberedTextField(
-//    value: String,
-//    onValueChange: (String) -> Unit,
-//    modifier: Modifier = Modifier,
-//    lineNumberWidth: Dp = 32.dp
-//) {
-//    // Split the text value into lines
-//    val lines = value.lines()
-//
-//    // Create a lazy column with the line numbers
-//    LazyColumn(
-//        modifier = modifier.fillMaxHeight(0.6.toFloat())
-//    ) {
-//        items(lines.size) { index ->
-//            Box(
-//                modifier = Modifier
-//                    .heightIn(min = 24.dp)
-//                    .widthIn(min = 24.dp)
-//                    .padding(end = 8.dp),
-//                contentAlignment = Alignment.CenterEnd
-//            ) {
-//                Text(
-//                    text = "${index + 1}",
-//                    style = MaterialTheme.typography.body2,
-//                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
-//                )
-//            }
-//        }
-//    }
-//
-//    // Create the text field with a modifier to shift the text to the right
-//    TextField(
-//        value = value,
-//        onValueChange = onValueChange,
-//        modifier = modifier
-//            .fillMaxHeight(0.6.toFloat())
-//    )
-//}
+    private fun checkIsSeparator(letter: Char): Boolean {
+        return separators.contains(letter) || letter.isWhitespace()
+    }
+
+    /**
+     * Highlighting keyWords ands double quotes in text.
+     * It can easily be extended to highlight single quotes, comments, etc
+     * @param codeText Source code, which should be highlighted
+     * @return TransformedText of highlighted code
+     */
+    fun highlightCode(codeText: AnnotatedString): TransformedText {
+        val anString = AnnotatedString.Builder().run {
+            append(codeText)
+            var prevQuote = -1;
+            for (currentPosition in codeText.indices) {
+                if (codeText[currentPosition] == '"') {
+                    if (prevQuote != -1) {
+                        addStyle(SpanStyle(color = Color.Yellow), prevQuote, currentPosition + 1)
+                        prevQuote = -1
+                    } else {
+                        prevQuote = currentPosition
+                    }
+                }
+                if (prevQuote != -1) { // we are inside string, no other highlight
+                    continue
+                }
+                if (currentPosition != codeText.length - 1 && !checkIsSeparator(codeText[currentPosition + 1])) {
+                    // checking if current symbol is end of word
+                    continue
+                }
+                for (keyWord in keyWords) {
+                    val wordBeginPos = currentPosition - keyWord.length + 1
+                    if (wordBeginPos < 0 || (wordBeginPos > 0 && !checkIsSeparator(codeText[wordBeginPos - 1]))) {
+                        // checking if word fits and there are a separator before it
+                        continue
+                    }
+                    if (codeText.substring(wordBeginPos, currentPosition + 1) == keyWord) {
+                        addStyle(SpanStyle(color = Color.Blue), wordBeginPos, currentPosition + 1)
+                    }
+                }
+            }
+            toAnnotatedString()
+        }
+        return TransformedText(anString, OffsetMapping.Identity)
+    }
+}
 
 
 @Composable
 @Preview
 fun App() {
-    val outputFile = File("output.txt")
-    val errorsFile = File("errors.txt")
-
-    var code by remember { mutableStateOf("") }
-    var output by remember { mutableStateOf(outputFile.readText()) }
-    val output_scroll = rememberScrollState(0)
-    val output_scroll_v = rememberScrollState(0)
-    var errors by remember { mutableStateOf(errorsFile.readText()) }
-    val errors_scroll = rememberScrollState(0)
-    val errors_scroll_v = rememberScrollState(0)
+    val outputFile = File("src/script_files/output.txt")
+    val errorsFile = File("src/script_files/errors.txt")
     val kotlinScriptFile = File("src/script_files/script.kts")
-    val process =
-        ProcessBuilder("bash", "kotlinc", "-script", kotlinScriptFile.absolutePath).redirectOutput(
-            outputFile
-        ).redirectError(errorsFile)
-    var cur_proces: Process? by remember { mutableStateOf(null) }
+
+    val codeTransformer = CodeTranformer()
+    var codeText by remember { mutableStateOf("") }
+
+    var output by remember { mutableStateOf(outputFile.readText()) }
+    val outputScrollHorizontal = rememberScrollState(0)
+    val outputScrollVertical = rememberScrollState(0)
+    var errors by remember { mutableStateOf(errorsFile.readText()) }
+    val errorsScrollHorizontal = rememberScrollState(0)
+    val errorsScrollVertical = rememberScrollState(0)
+
     var exitCode: Int? by remember { mutableStateOf(null) }
-    var runningStatus : String? by remember { mutableStateOf(null) }
-    var curProcessThread: Thread? = null
-    LaunchedEffect(outputFile) {
+    var runningStatus: String by remember { mutableStateOf("") }
+    val process =
+        ProcessBuilder("bash", "kotlinc", "-script", kotlinScriptFile.absolutePath)
+            .redirectOutput(outputFile)
+            .redirectError(errorsFile)
+    var scriptThread: Thread? = null
+    var scriptProcess: Process? = null
+
+
+    LaunchedEffect(outputFile) {// updating data from output file
         while (true) {
             val fileContents = outputFile.readText()
             output = fileContents
@@ -88,7 +109,7 @@ fun App() {
         }
     }
 
-    LaunchedEffect(errorsFile) {
+    LaunchedEffect(errorsFile) { // updating date from error file
         while (true) {
             val fileContents = errorsFile.readText()
             errors = fileContents
@@ -97,18 +118,25 @@ fun App() {
     }
     Column(modifier = Modifier.fillMaxSize()) {
         Row(modifier = Modifier.fillMaxHeight(0.9.toFloat())) {
+            // Code editor column
             Column(modifier = Modifier.fillMaxWidth(0.6.toFloat()).padding(20.dp)) {
+                // TopBar with info
                 TopAppBar(modifier = Modifier.fillMaxHeight(0.1.toFloat())) {
                     Text("Code:")
                 }
+                // Code editor
                 TextField(
-                    value = code, onValueChange = { code = it }, Modifier
+                    value = codeText, onValueChange = { codeText = it }, Modifier
                         .fillMaxSize()
                         .border(BorderStroke(1.dp, Color.Black)),
-                    label = {}
+                    label = {},
+                    visualTransformation = { codeTransformer.highlightCode(it) }
                 )
+
             }
+            // Right column with Output and Error
             Column(modifier = Modifier.padding(20.dp)) {
+                // Output
                 Column(modifier = Modifier.fillMaxHeight(0.5.toFloat())) {
                     TopAppBar(modifier = Modifier.fillMaxHeight(0.1.toFloat())) {
                         Text(
@@ -118,10 +146,12 @@ fun App() {
                     Text(
                         text = output,
                         modifier = Modifier.fillMaxSize()
-                            .horizontalScroll(output_scroll).verticalScroll(output_scroll_v)
+                            .horizontalScroll(outputScrollHorizontal)
+                            .verticalScroll(outputScrollVertical)
                             .border(BorderStroke(1.dp, Color.Black))
                     )
                 }
+                // Errors
                 Column(modifier = Modifier.fillMaxHeight()) {
                     TopAppBar(modifier = Modifier.fillMaxHeight(0.1.toFloat())) {
                         Text(
@@ -131,60 +161,76 @@ fun App() {
                     Text(
                         text = errors,
                         modifier = Modifier.fillMaxHeight().fillMaxWidth()
-                            .horizontalScroll(errors_scroll).verticalScroll(errors_scroll_v)
+                            .horizontalScroll(errorsScrollHorizontal)
+                            .verticalScroll(errorsScrollVertical)
                             .border(BorderStroke(1.dp, Color.Black))
                     )
-
                 }
-
-
             }
 
         }
-
+        // Bottom bar with buttons and info about current running state
         BottomAppBar(modifier = Modifier.fillMaxHeight()) {
-            Row {
-                Text(text = "Run")
-
+            // Buttons
+            Row(modifier = Modifier.fillMaxWidth(0.2.toFloat())) {
+                // RUN button
                 Button(onClick = {
-                    curProcessThread?.interrupt()
-                    runningStatus = null
-                    cur_proces?.destroy()
-                    curProcessThread = Thread {
+                    scriptThread?.interrupt()
+                    scriptProcess?.destroy()
+                    scriptThread = Thread {
                         runningStatus = "Running"
                         val writer = PrintWriter(kotlinScriptFile);
-                        writer.print(code);
+                        writer.print(codeText);
                         writer.close()
-                        cur_proces = process.start()
-                        exitCode = cur_proces!!.waitFor()
-                        println(exitCode)
-                        runningStatus = null
+                        try {
+                            scriptProcess = process.start()
+                            exitCode = scriptProcess!!.waitFor()
+                            runningStatus = "Process finished"
+                        } catch (e: InterruptedException) {
+                            println("Script was interrupted")
+                        }
                     }
-                    curProcessThread?.start()
+                    scriptThread?.start()
                 }, content = { Text(text = "RUN") })
-
+                // STOP button
                 Button(onClick = {
-                    curProcessThread?.interrupt()
-                    cur_proces?.destroy()
-                    runningStatus = null
+                    scriptThread?.interrupt()
+                    scriptProcess?.destroy()
+                    if (runningStatus == "Running") {
+                        runningStatus = "Process Interrupted"
+                    }
                 }, content = { Text(text = "STOP") })
             }
-            Text(
-                text = runningStatus ?: ""
-            )
-            Text(
-                text = if (exitCode == null) {
-                    ""
-                } else {
-                    "Last return code is " + exitCode
+            // Info about script state
+            Row(modifier = Modifier.fillMaxSize()) {
+                // Running status
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxWidth(0.5.toFloat()).fillMaxHeight()
+                ) {
+                    Text(
+                        text = runningStatus
+                    )
                 }
-            )
+                // Last return code
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = if (exitCode == null) {
+                            ""
+                        } else {
+                            "Last return code is " + exitCode
+                        }
+                    )
+                }
+            }
         }
-
     }
-
 }
 
+// Main just running out application
 fun main() = application {
     Window(title = "RUN", onCloseRequest = ::exitApplication) {
         App()
---
+++ src/script_files/script.kts
@@ -0,0 +1,8 @@
+fun pr() {
+  var st = "string"
+		var c = 100
+		while (c-- != 0) {
+				println(st)
+		}
+}
+pr()
